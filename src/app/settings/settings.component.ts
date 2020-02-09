4db7d6ce909f6d12bdb96bb1b03197383581d7b3
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-02-09 00:51:27 -0500

    New settings component for a future release

---
+++ src/app/settings/settings.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './settings.component.html'
+})
+export class SettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
