872f7e8b4a64672e4c4fc2a60ff7b41bc50459dc
Felipe Triana <w.trianag@uniandes.edu.co>
2020-02-09 20:10:54 -0500

    change routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from "./login/login.component";
 import { AuthGuard } from "./helpers/auth.guard";
 import { HomeComponent } from "./home/home.component";
 import { SettingsComponent } from "./settings/settings.component";
+import { UsersComponent } from "./users/users.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
+  {path: 'users', component: UsersComponent},
   {path: 'settings', component: SettingsComponent, canActivate: [AuthGuard]},
   {path: '**', redirectTo: ''}
 ];
